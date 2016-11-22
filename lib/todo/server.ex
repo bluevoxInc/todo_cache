7e8585812f793eeaafe0b3fd66410266e3057a10
wnorman <bluevox.inc@gmail.com>
2016-11-22 17:47:59 -0500

    add database server code

--- lib/todo/server.ex
+++ lib/todo/server.ex
@@ -1,8 +1,8 @@
 defmodule Todo.Server do
   use GenServer
 
-  def start do
-    GenServer.start(Todo.Server, nil)
+  def start(list_name) do
+    GenServer.start(Todo.Server, list_name)
   end
 
   def add_entry(todo_server, new_entry) do
@@ -21,30 +21,39 @@ defmodule Todo.Server do
     GenServer.call(todo_server, {:all_entries})
   end
 
-  def init(_) do
-    {:ok, Todo.List.new}
+  def init(name) do
+    send(self, :real_init) #send first msg to handle data load
+    {:ok, {name, nil}}
   end
 
-  def handle_cast({:add_entry, new_entry}, todo_list) do
+  def handle_cast({:add_entry, new_entry}, {name, todo_list}) do
     new_state = Todo.List.add_entry(todo_list, new_entry)
-    {:noreply, new_state}
+    Todo.Database.store(name, new_state)
+    {:noreply, {name, new_state}}
   end
 
-  def handle_cast({:delete_entry, id}, todo_list) do
+  def handle_cast({:delete_entry, id}, {name, todo_list}) do
     new_state = Todo.List.delete_entry(todo_list, id)
-    {:noreply, new_state}
+    Todo.Database.store(name, new_state)
+    {:noreply, {name, new_state}}
   end
 
-  def handle_call({:entries, date}, _, todo_list) do
+  def handle_call({:entries, date}, _, {name, todo_list}) do
     {
       :reply,
       Todo.List.entries(todo_list, date),
-      todo_list
+      {name, todo_list}
     }
   end
 
-  def handle_call({:all_entries}, _, todo_list) do
-    {:reply, Todo.List.all_entries(todo_list), todo_list}
+  def handle_call({:all_entries}, _, {name, todo_list}) do
+    {:reply, Todo.List.all_entries(todo_list), {name, todo_list}}
   end
+
+  #handle the data load here so as not to block GenServer init/start
+  def handle_info(:real_init, {name, _}) do
+    {:noreply, {name, Todo.Database.get(name) || Todo.List.new}}
+  end
+
 end
 
