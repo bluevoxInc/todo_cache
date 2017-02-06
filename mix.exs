1de072e1524daf42a9d4913ad3d2335241ad624e
wnorman <bluevox.inc@gmail.com>
2017-02-06 11:17:42 -0500

    add libring

--- mix.exs
+++ mix.exs
@@ -15,7 +15,7 @@ defmodule Todo.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     [
-      applications: [:logger, :gproc, :cowboy, :plug, :mnesia],
+      applications: [:logger, :gproc, :cowboy, :plug, :mnesia, :libring],
       mod: {Todo.Application, []},
       env: []
     ]
@@ -35,6 +35,7 @@ defmodule Todo.Mixfile do
       {:gproc, "~> 0.5.0"},
       {:cowboy, "~> 1.0.4"},
       {:plug, "~> 1.3.0"},
+      {:libring, github: "bitwalker/libring", tag: :master},
       {:meck, "~> 0.8.3", only: :test},
       {:httpoison, "~> 0.10.0", only: :test}
     ]
